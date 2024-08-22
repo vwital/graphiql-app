be18fffb8c25814296dd37c7fdc07e43023ce820
Kiryl Panamarou <70377995+panakir@users.noreply.github.com>
2024-08-22 15:07:06 +0300

    RC-5 Implment header component, RC-6 Implement footer component (#2)
    * style: add normalize.css
    
    * RC-5 feat: implement header
    
    * RC-6 implement footer component, fix layout component
    
    * style: add styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./header.module.scss";
+import { useLayoutEffect, useState } from "react";
+
 const Header = (): React.ReactNode => {
-  return <header>Hello, world!</header>;
+  const [isSticky, setIsSticky] = useState(false);
+
+  const setSticked = (): void => {
+    if (window.scrollY > 50) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("scroll", setSticked);
+    return (): void => window.removeEventListener("scroll", setSticked);
+  }, []);
+
+  return (
+    <>
+      <header
+        className={`${styles.header} ${isSticky ? `${styles.header_sticky}` : null}`}
+      >
+        <div className={styles.header__wrapper}>
+          <h1>REST/GraphiQL Client</h1>
+          <div className={styles.header__controls}>
+            <button type="button">en</button>
+            <div className={styles.header__links}>
+              <Link href={"/sign-in"}>login</Link>
+              <Link href={"/sign-up"}>register</Link>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;

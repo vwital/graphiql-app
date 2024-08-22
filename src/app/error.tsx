35f868045f37ce8ee71592f416b94f4b0e77a0c6
Kiryl Panamarou <70377995+panakir@users.noreply.github.com>
2024-08-22 15:13:09 +0300

    RC-7 feat: implement base routing (#1)

---
+++ src/app/error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const ErrorPage = (): React.ReactNode => {
+  return <h2>ErrorPage</h2>;
+};
+
+export default ErrorPage;
---
+++ src/app/graphi/page.tsx
@@ -0,0 +1,5 @@
+const GraphiQlClientPage = (): React.ReactNode => {
+  return <h2>GraphiQL Client Page</h2>;
+};
+
+export default GraphiQlClientPage;

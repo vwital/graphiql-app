d378c7cb0f6fed0d21cbfec2c34cf79287ad7cf0
panakir <panakir.webdev@gmail.com>
2024-08-31 22:15:25 +0300

    RC-14 feat: implement dynamic fields (headers and body fields) in form

--- src/components/restForm/RestForm.tsx
+++ src/components/restForm/RestForm.tsx
@@ -1,12 +1,56 @@
+"use client";
+
 import { useTranslations } from "next-intl";
 import styles from "./restForm.module.scss";
+import { useFieldArray, useForm } from "react-hook-form";
+
+type FormValues = {
+  method: string;
+  url: string;
+  headers: {
+    key: string;
+    value: string;
+  }[];
+  body: {
+    value: string;
+  }[];
+};
 
 const RestForm = (): React.ReactNode => {
   const t = useTranslations("RestClientPage");
+  const { register, handleSubmit, control } = useForm<FormValues>({
+    mode: "onSubmit",
+    defaultValues: {},
+  });
+
+  const {
+    fields: headersFields,
+    append: headersAppend,
+    remove: headersRemove,
+  } = useFieldArray({
+    name: "headers",
+    control,
+  });
+  const {
+    fields: bodyFields,
+    append: bodyAppend,
+    remove: bodyRemove,
+  } = useFieldArray({
+    name: "body",
+    control,
+  });
+
+  const onSubmit = (data: FormValues): FormValues => {
+    return data;
+  };
+
   return (
     <>
       <h2>{t("title")}</h2>
-      <form className={styles.form}>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className={styles.form__row}>
           <div className={styles.form__field}>
             <label
@@ -16,6 +60,7 @@ const RestForm = (): React.ReactNode => {
               {t("method")}
             </label>
             <select
+              {...register("method")}
               className="select"
               name="method"
               id="method"
@@ -34,38 +79,98 @@ const RestForm = (): React.ReactNode => {
               {t("url")}
             </label>
             <input
+              {...register("url")}
               className="input"
               type="text"
               name="url"
               id="url"
               placeholder="https://example.com"
+              value={"https://example.com"}
             />
           </div>
         </div>
-        <div className={styles.form__row + " " + styles.form__row_header}>
-          <label className={styles.form__label}>{t("header")}</label>
-          <div className={styles.form__field_wrapper}>
-            <input
-              className="input"
-              type="text"
-              name="headers"
-              id="headerKey"
-              placeholder="Header key"
-            />
-            <input
-              className="input"
-              type="text"
-              name="headers"
-              id="headerValue"
-              placeholder="Header value"
-            />
-          </div>
+        <div className={styles.form__headers + " " + styles.headers}>
+          {headersFields.map((field, index) => {
+            return (
+              <div
+                key={field.id}
+                className={styles.headers__row}
+              >
+                <label className={styles.headers__label}>
+                  <span>Header key</span>
+                  <input
+                    {...register(`headers.${index}.key`)}
+                    className={`${styles.headers__input} input`}
+                    type="text"
+                    name="headerKey"
+                    id="headerKey"
+                  />
+                </label>
+                <label className={styles.headers__label}>
+                  <span>Header value</span>
+                  <input
+                    {...register(`headers.${index}.value`)}
+                    className={`${styles.headers__input} input`}
+                    type="text"
+                    name="headerValue"
+                    id="headerValue"
+                  />
+                </label>
+                <button
+                  className={`${styles.headers__button} ${styles.headers__button_delete}  button`}
+                  type="button"
+                  onClick={() => headersRemove(index)}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
           <button
-            type="button"
             className={styles.form__button + " button"}
+            type="button"
+            onClick={() => headersAppend({ key: "", value: "" })}
           >
             {t("addHeader")}
           </button>
+        </div>
+        <div className={`${styles.form__body} ${styles.form__body} `}>
+          {bodyFields.map((field, index) => (
+            <div
+              key={field.id}
+              className={styles.form__row}
+            >
+              <label className={styles.headers__label}>
+                <span>Body</span>
+                <textarea
+                  {...register(`body.${index}.value`)}
+                  key={field.id}
+                  name={`body.${index}.value`}
+                  id={`body.${index}.value`}
+                  rows={10}
+                />
+              </label>
+            </div>
+          ))}
+          <div className={styles.body__buttons}>
+            <button
+              className="button"
+              type="button"
+              disabled={bodyFields.length === 1}
+              onClick={() => bodyAppend({ value: "" })}
+            >
+              Add body
+            </button>
+            {bodyFields.length === 1 ? (
+              <button
+                className="button"
+                type="button"
+                onClick={() => bodyRemove(0)}
+              >
+                Delete body
+              </button>
+            ) : null}
+          </div>
         </div>
         <button
           type="submit"

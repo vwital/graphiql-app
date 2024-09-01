d88eb4736f8eb11368b2d19f2fab7c639ff9199a
dzehil02 <dzehil02@gmail.com>
2024-09-01 20:47:56 +0200

    RC-9 feat: add sign-in/up route protection add redirect to main page for logged user

--- src/app/[locale]/(auth)/sign-in/page.tsx
+++ src/app/[locale]/(auth)/sign-in/page.tsx
@@ -2,11 +2,29 @@
 import { useTranslations } from "next-intl";
 import styles from "../sign.module.scss";
 import LoginForm from "@/components/forms/LoginForm";
+import { useRouter } from "@/navigation";
+import { useAuth } from "@/hooks/useAuth";
+import { useEffect, useState } from "react";
 
 const SignInPage = (): React.ReactNode => {
   const t = useTranslations("SignInPage");
+  const router = useRouter();
+  const { isAuth } = useAuth();
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
 
-  return (
+  useEffect(() => {
+    if (isAuth) {
+      router.replace("/");
+    } else {
+      setIsCheckingAuth(false);
+    }
+  }, [isAuth, router]);
+
+  if (isCheckingAuth) {
+    return <div>{t("loading")}</div>;
+  }
+
+  return isAuth ? null : (
     <div className={styles.sign}>
       <div className={styles.sign__block}>
         <h2>{t("signIn")}</h2>
--- src/app/[locale]/(auth)/sign-up/page.tsx
+++ src/app/[locale]/(auth)/sign-up/page.tsx
@@ -2,11 +2,29 @@
 import { useTranslations } from "next-intl";
 import styles from "../sign.module.scss";
 import RegForm from "@/components/forms/RegForm";
+import { useAuth } from "@/hooks/useAuth";
+import { useState, useEffect } from "react";
+import { useRouter } from "@/navigation";
 
 const SingUpPage = (): React.ReactNode => {
   const t = useTranslations("SignUpPage");
+  const router = useRouter();
+  const { isAuth } = useAuth();
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
 
-  return (
+  useEffect(() => {
+    if (isAuth) {
+      router.replace("/");
+    } else {
+      setIsCheckingAuth(false);
+    }
+  }, [isAuth, router]);
+
+  if (isCheckingAuth) {
+    return <div>{t("loading")}</div>;
+  }
+
+  return isAuth ? null : (
     <div className={styles.sign}>
       <div className={styles.sign__block}>
         <h2>{t("signUp")}</h2>

757d0748fe5d33151832e91bebc9c5da9b608a7b
panakir <panakir.webdev@gmail.com>
2024-09-02 13:05:26 +0300

    RC-14 refactor: refactor convertBase64 functions

--- src/utils/convertBase64.ts
+++ src/utils/convertBase64.ts
@@ -1,13 +1,6 @@
-const convertToBase64 = (url: string): string => {
-  const data = JSON.stringify({ url });
-  const buff = Buffer.from(data);
-  return buff.toString("base64");
-};
+const convertToBase64 = (url: string): string => btoa(url);
 
-const convertFromBase64 = (base64: string): string => {
-  const buff = Buffer.from(base64, "base64");
-  const data = buff.toString("utf8");
-  return JSON.parse(data).url;
-};
+const convertFromBase64 = (base64: string | string[]): string =>
+  atob(base64.toString().replace(/%3D{1,}/g, ""));
 
 export { convertFromBase64, convertToBase64 };

25e94ffbfe4d6300da95db00341674b3afc5b183
panakir <panakir.webdev@gmail.com>
2024-09-02 13:08:39 +0300

    RC-14 feat: implement application url change when selecting method and url

--- src/components/restForm/RestForm.tsx
+++ src/components/restForm/RestForm.tsx
@@ -3,21 +3,22 @@
 import { useTranslations } from "next-intl";
 import styles from "./restForm.module.scss";
 import { useFieldArray, useForm } from "react-hook-form";
+import { usePathname, useRouter } from "@/navigation";
+import { useParams } from "next/navigation";
+import { convertFromBase64, convertToBase64 } from "@/utils/convertBase64";
 
 type FormValues = {
   method: string;
   url: string;
-  headers: {
-    key: string;
-    value: string;
-  }[];
-  body: {
-    value: string;
-  }[];
+  headers: { key: string; value: string }[];
+  body: { value: string }[];
 };
 
 const RestForm = (): React.ReactNode => {
   const t = useTranslations("RestClientPage");
+  const router = useRouter();
+  const urlParams = useParams();
+  const pathname = usePathname();
   const { register, handleSubmit, control } = useForm<FormValues>({
     mode: "onSubmit",
     defaultValues: {},
@@ -31,6 +32,7 @@ const RestForm = (): React.ReactNode => {
     name: "headers",
     control,
   });
+
   const {
     fields: bodyFields,
     append: bodyAppend,
@@ -39,6 +41,40 @@ const RestForm = (): React.ReactNode => {
     name: "body",
     control,
   });
+
+  const handleMethodChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
+    const newPathname = urlParams.method
+      ? pathname.replace(`/${urlParams.method}`, `/${event.target.value}`)
+      : `${pathname}/${event.target.value}`;
+
+    router.push(newPathname);
+  };
+
+  const handleUrlChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const base64 = convertToBase64(event.target.value);
+    if (event.target.value === "") {
+      return;
+    }
+    if (!urlParams.requestUrl) {
+      const newPath = urlParams.method
+        ? `${pathname}/${base64}`
+        : `${pathname}/GET/${base64}`;
+      router.push(newPath);
+    } else {
+      const newPath = pathname.split("/").slice(0, -1).join("/");
+      router.push(`${newPath}/${base64}`);
+    }
+  };
+
+  const handleDefValue = (): string => {
+    const valueBase64 = urlParams.requestUrl ? urlParams.requestUrl : "";
+    if (!valueBase64) return "";
+    return convertFromBase64(valueBase64);
+  };
 
   const onSubmit = (data: FormValues): FormValues => {
     return data;
@@ -64,11 +100,24 @@ const RestForm = (): React.ReactNode => {
               className="select"
               name="method"
               id="method"
+              onChange={(event) => {
+                handleMethodChange(event);
+              }}
+              defaultValue={urlParams.method || "default"}
             >
+              <option
+                disabled
+                value="default"
+              >
+                Choose method
+              </option>
               <option value="GET">GET</option>
               <option value="POST">POST</option>
               <option value="PUT">PUT</option>
               <option value="DELETE">DELETE</option>
+              <option value="PATCH">PATCH</option>
+              <option value="HEAD">HEAD</option>
+              <option value="OPTIONS">OPTIONS</option>
             </select>
           </div>
           <div className={styles.form__field + " " + styles.form__field_url}>
@@ -84,8 +133,11 @@ const RestForm = (): React.ReactNode => {
               type="text"
               name="url"
               id="url"
+              defaultValue={handleDefValue()}
               placeholder="https://example.com"
-              value={"https://example.com"}
+              onBlur={(event) => {
+                handleUrlChange(event);
+              }}
             />
           </div>
         </div>

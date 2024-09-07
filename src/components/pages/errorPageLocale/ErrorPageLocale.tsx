78274c23d55aa43f93d493d42275028193699891
dzehil02 <dzehil02@gmail.com>
2024-09-07 18:19:18 +0200

    RC-19 feat: add error.tsx

---
+++ src/components/pages/errorPageLocale/ErrorPageLocale.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useTranslations } from "next-intl";
+import { Link } from "@/navigation";
+import styles from "./styles.module.scss";
+
+const ErrorPage = ({ error }: { error: Error }): JSX.Element => {
+  const t = useTranslations("ErrorPage");
+
+  return (
+    <div className={styles["error-page"]}>
+      <h1 className={styles["error-page__title"]}>{t("errorHeader")}</h1>
+      <h2 className={styles["error-page__subtitle"]}>{t("errorMessage")}</h2>
+
+      <p className={styles["error-page__text"]}>
+        {t("error")}: {error.message}
+      </p>
+
+      <Link
+        href="/"
+        className="button"
+      >
+        {t("errorButton")}
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;

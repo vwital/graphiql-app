aced583de989859b738f3d617005403307894422
panakir <panakir.webdev@gmail.com>
2024-09-13 11:37:16 +0300

    RC-14 feat: add body request to server action

--- src/app/actions/restClientFormAction.ts
+++ src/app/actions/restClientFormAction.ts
@@ -3,16 +3,26 @@
 const restClientFormAction = async (
   _previousState: unknown,
   data: FormData
-): Promise<object> => {
-  const { url, method } = Object.fromEntries(data);
+): Promise<unknown> => {
+  const { url, method, ...props } = Object.fromEntries(data);
+  const body = props["body.0.value"];
+
   const options = {
     method: method.toString(),
+    body: body ?? null,
+    headers: {
+      "Content-Type": "application/json",
+    },
   };
 
   const response = await fetch(url.toString(), options);
   const dataFromResponse = await response.json();
 
-  return { dataFromResponse, statusCode: response.status };
+  return {
+    dataFromResponse,
+    statusCode: response.status,
+    statusText: response.statusText,
+  };
 };
 
 export default restClientFormAction;

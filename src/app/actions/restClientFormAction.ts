07c051ace7a34a9b8f5dde214d9637ced5a5c988
Kiryl Panamarou <70377995+panakir@users.noreply.github.com>
2024-09-15 01:17:33 +0300

    RC-14 Implement the functionality of the REST client page (#15)
    * fix: fix header sticky flicker bug
    
    * RC-11 feat: implement static layout Rect Client Page
    
    * refactor: replace routes folders to [locale]
    
    * RC-11 feat: implement Rest client page  localization
    
    * RC-14 feat: implement dynamic fields (headers and body fields) in form
    
    * RC-14 feat: implement function to convert base64
    
    * RC-16 refactor: replace pages folder into components folder
    
    * RC-14 feat: add dynamic routes
    
    * RC-14 refactor: refactor convertBase64 functions
    
    * RC-14 feat: implement application url change when selecting method and url
    
    * RC-14 styles: add error class selector, add color-error variable
    
    * RC-14 feat: implement body field in RestForm, add buttons for add, delete, pretty body field, implement change application url when body field focus out
    
    * RC-14 refactor: replace restForm and restResponse components into restClient/components
    
    * RC-14 feat: add translator for buttons
    
    * RC-14 refactor: add constants file, separate variable
    
    * RC-14 feat: implement body buttons functionality,  refactor  method field
    
    * RCC-14 feat: add translator for  headers and body fields
    
    * RC-14 feat: implement add header key/value to url query
    
    * RC-14 fix: fix body fields naming and default value
    
    * RC-14 feat: add  and setup redux
    
    * RC-14 feat: implement base server actions request and display response component
    
    * refactor: delete unused files, delete comment in StoreProvider
    
    * RC-14 refactor: replace slice file in app/lib/features folder
    
    * fix: fix StoreProvider boundary
    
    * chore: update package-lock file
    
    * RC-14 feat: add check request for response display
    
    * RC-14 feat: add submitButton  component, add translation for it
    
    * RC-14 refactor: separate json viewer into a specific  component
    
    * RC-14 feat: update method change (display body field according to method), implement  isDisabledAddBodyButton function
    
    * RC-14 fix: fix bugs in body field, setup disabling body buttons, separate the function handleDefaultValue  into a specific file
    
    * RC-14 style: add form adaptive style
    
    * refactor: rename style file for json viewer component
    
    * RC-14 refactor: change body wrapper  className
    
    * RC-14 feat: add body request to server action
    
    * fix: fix lint and format scripts
    
    * RC-14 feat: implement add headers to request
    
    * refactor: delete unused files
    
    * RC-14 fix: bugfix window not defined issue
    
    * RC-14 feat: implement removing header
    
    * RC-14 feat: remove delete header button
    
    * RC-14 fix: fix error during HEAD and OPTIONS methods
    
    ---------
    
    Co-authored-by: Vital <86975341+vwital@users.noreply.github.com>

--- src/app/actions/restClientFormAction.ts
+++ src/app/actions/restClientFormAction.ts
@@ -4,19 +4,45 @@ const restClientFormAction = async (
   _previousState: unknown,
   data: FormData
 ): Promise<unknown> => {
-  const { url, method, ...props } = Object.fromEntries(data);
-  const body = props["body.0.value"];
+  const {
+    url,
+    method,
+    "body.0.value": body,
+    ...headers
+  } = Object.fromEntries(data);
 
+  const createHeader = (): Record<string, string> => {
+    const result: Record<string, string> = {};
+    const headerValues = Object.values(headers);
+    for (let i = 0; i < headerValues.length; i++) {
+      if (i % 2 === 0) {
+        result[`${headerValues[i]}`] = `${headerValues[i + 1]}`;
+      }
+    }
+    return result;
+  };
+  
   const options = {
     method: method.toString(),
     body: body ?? null,
     headers: {
-      "Content-Type": "application/json",
+      ...createHeader()
     },
   };
 
   const response = await fetch(url.toString(), options);
-  const dataFromResponse = await response.json();
+  const contentType = response.headers.get("content-type");
+  let dataFromResponse;
+
+  if (contentType === "application/json") {
+    try {
+      dataFromResponse = await response.json();
+    } catch {
+      dataFromResponse = {};
+    }
+  } else {
+    dataFromResponse = (await response.text()).split("\n");
+  }
 
   return {
     dataFromResponse,

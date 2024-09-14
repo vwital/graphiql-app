b3f125cc750ed755136ea1eae529c381e1bec70a
dzehil02 <dzehil02@gmail.com>
2024-09-14 13:47:18 +0200

    RC-20 fix: test for graphi page

--- src/tests/app/locale/graphi/graphi-page.test.tsx
+++ src/tests/app/locale/graphi/graphi-page.test.tsx
@@ -2,6 +2,10 @@ import GraphiQlClientPage from "@/app/[locale]/graphi/page";
 import { IntlProviderWrapper } from "@/tests/utils/test-utils";
 import { render, screen } from "@testing-library/react";
 
+vi.mock("@/navigation", () => ({
+  useRouter: () => ({ replace: vi.fn() }),
+}));
+
 describe("GraphiQlClientPage", () => {
   it("should render the correct header text and button text", () => {
     render(
@@ -14,9 +18,9 @@ describe("GraphiQlClientPage", () => {
     expect(screen.getByText("Endpoint URL")).toBeInTheDocument();
     expect(screen.getByText("Headers")).toBeInTheDocument();
     expect(screen.getByText("Query")).toBeInTheDocument();
-    expect(screen.getByText("Variables")).toBeInTheDocument();
     expect(
       screen.getByRole("button", { name: "Add header" })
     ).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
   });
 });

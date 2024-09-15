014157cdb41b42efddadcfa0ee9e93f7d0b05bae
panakir <panakir.webdev@gmail.com>
2024-09-16 01:15:16 +0300

    refactor: refactor header component

---
+++ src/components/layouts/header/constants.ts
@@ -0,0 +1,14 @@
+const STICKY_SCROLL = 60;
+
+const NOT_AUTH_LINKS = [
+  {
+    name: "login",
+    href: "/sign-in",
+  },
+  {
+    name: "register",
+    href: "/sign-up",
+  },
+];
+
+export { STICKY_SCROLL, NOT_AUTH_LINKS };

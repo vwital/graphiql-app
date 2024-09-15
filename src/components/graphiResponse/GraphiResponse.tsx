9a4a943aff589f842846de5a6e607fe582eda517
dzehil02 <dzehil02@gmail.com>
2024-09-15 11:25:54 +0200

    fix: Uncommented GraphiResponse to build project

--- src/components/graphiResponse/GraphiResponse.tsx
+++ src/components/graphiResponse/GraphiResponse.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { useTranslations } from "next-intl";
 import styles from "./response.module.scss";
-// import { useSelector } from "react-redux";
-// import { RootState } from "@/app/lib/store";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/lib/store";
 import { useParams } from "next/navigation";
-// import JsonViewer from "@/components/JsonViewer/JsonViewer";
+import JsonViewer from "@/components/JsonViewer/JsonViewer";
 
 const RestResponse = (): React.ReactNode => {
   const t = useTranslations("RestClientPage");
-  // const [response] = useSelector((state: RootState) => state.restClient);
+  const [response] = useSelector((state: RootState) => state.restClient);
   const params = useParams();
   if (!params.requestUrl) return null;
 

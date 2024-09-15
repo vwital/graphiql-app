2ffe071425026416e3b0efe5b44e84bcfed5047c
panakir <panakir.webdev@gmail.com>
2024-09-16 00:47:09 +0300

    refactor: refactor AuthorsSlider component

--- src/components/pages/welcomePage/components/author-card/authorsInfo.ts
+++ src/components/pages/welcomePage/components/author-card/authorsInfo.ts
@@ -1,35 +1,30 @@
 import { IAuthorInfo } from "@/interfaces/interfaces";
 import { useTranslations } from "next-intl";
 
-export const viwtal = (): IAuthorInfo => {
-  const t = useTranslations("Vwital");
+const authors = (): { [key: string]: IAuthorInfo } => {
+  const t = useTranslations("Author");
   return {
-    nickname: "vwital",
-    name: t("name"),
-    role: t("role"),
-    bio: t("bio"),
-    github: "https://github.com/vwital",
+    vwital: {
+      nickname: "vwital",
+      name: t("vwitalName"),
+      role: t("vwitalRole"),
+      bio: t("vwitalBio"),
+      github: "https://github.com/vwital",
+    },
+    panakir: {
+      nickname: "panakir",
+      name: t("panakirName"),
+      role: t("panakirRole"),
+      bio: t("panakirBio"),
+      github: "https://github.com/panakir",
+    },
+    dzehil02: {
+      nickname: "dzehil02",
+      name: t("dzehil02Name"),
+      role: t("dzehil02Role"),
+      bio: t("dzehil02Bio"),
+      github: "https://github.com/dzehil02",
+    },
   };
 };
-
-export const panakir = (): IAuthorInfo => {
-  const t = useTranslations("Panakir");
-  return {
-    nickname: "panakir",
-    name: t("name"),
-    role: t("role"),
-    bio: t("bio"),
-    github: "https://github.com/panakir",
-  };
-};
-
-export const dzehil02 = (): IAuthorInfo => {
-  const t = useTranslations("Dzehil02");
-  return {
-    nickname: "dzehil02",
-    name: t("name"),
-    role: t("role"),
-    bio: t("bio"),
-    github: "https://github.com/dzehil02",
-  };
-};
+export { authors };
--- src/components/pages/welcomePage/components/authors-slider/AuthorsSlider.tsx
+++ src/components/pages/welcomePage/components/authors-slider/AuthorsSlider.tsx
@@ -3,12 +3,13 @@ import AuthorCard from "../author-card/AuthorCard";
 import vwitalImg from "@/assets/img/vwital.jpg";
 import panakirImg from "@/assets/img/panakir.jpg";
 import dzehil02Img from "@/assets/img/dzehil02.jpg";
-import { viwtal, panakir, dzehil02 } from "../author-card/authorsInfo";
+import { authors } from "../author-card/authorsInfo";
 import styles from "./authors-slider.module.scss";
 import { useState } from "react";
 
 const AuthorsSlider = (): React.ReactNode => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const { vwital, panakir, dzehil02 } = authors();
   const slides = [
     {
       id: 1,
@@ -16,7 +17,7 @@ const AuthorsSlider = (): React.ReactNode => {
         <AuthorCard
           className={styles.slide}
           src={vwitalImg}
-          text={viwtal()}
+          text={vwital}
         ></AuthorCard>
       ),
     },
@@ -26,7 +27,7 @@ const AuthorsSlider = (): React.ReactNode => {
         <AuthorCard
           className={styles.slide}
           src={panakirImg}
-          text={panakir()}
+          text={panakir}
         ></AuthorCard>
       ),
     },
@@ -36,7 +37,7 @@ const AuthorsSlider = (): React.ReactNode => {
         <AuthorCard
           className={styles.slide}
           src={dzehil02Img}
-          text={dzehil02()}
+          text={dzehil02}
         ></AuthorCard>
       ),
     },
@@ -53,13 +54,11 @@ const AuthorsSlider = (): React.ReactNode => {
           className={styles.slides}
           style={{ transform: `translateX(-${currentSlide * 100}%)` }}
         >
-          {slides[0].elem}
-          {slides[1].elem}
-          {slides[2].elem}
+          {slides.map((slide) => slide.elem)}
         </div>
       </div>
       <div className={styles.slider__controls}>
-        {[0, 1, 2].map((index) => (
+        {slides.map((_, index) => (
           <button
             key={index}
             className={`${styles.slider__btn} ${currentSlide === index ? styles.slider__btn_active : ""} button`}
